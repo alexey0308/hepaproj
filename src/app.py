b0809ad13731d3ad38d06990cb1559c13ed07c07
Alexey <alexey.mipt@gmail.com>
2020-10-27 12:13:37 +0100

    add update of plot

--- src/app.py
+++ src/app.py
@@ -8,9 +8,11 @@ from plots import plotGene
 import dash_table
 from  dataimport import getCellAnno, getFracs, getGenes, getDETable
 import yaml
+from dash.dependencies import Input, Output
 
 with open("config.yaml") as file:
     config = yaml.safe_load(file)
+
 
 datadir = config["datadir"]
 
@@ -24,15 +26,28 @@ def x2labels(x):
 
 def cellTypeDrop():
     return dcc.Dropdown(
+        id="cellTypeSelector",
         options=[
             {'label':"hepatocytes", 'value': "hep"},
             {'label':"lsec", 'value': "lsec"},
-        ]
+        ], value="hep"
     )
 
-def geneListDrop(genes):
+@app.callback(
+    Output("geneHeader", "children"),
+    Input("geneSelector", "value")
+)
+def updateGeneHeader(gene):
+    return "Gene: {}".format(gene)
+
+
+def geneListDrop(genes, default=None):
+    if default is None:
+        default = genes[0]
     x = dcc.Dropdown(
-        options=[{"label": x, "value": x} for x in genes])
+        id="geneSelector",
+        options=[{"label": x, "value": x} for x in genes],
+        value=default)
     return x
 
 def resultTable(tbl):
@@ -59,17 +74,30 @@ for cellType in ["hep", "lsec"]:
 
 cellType = "hep"
 gene = "0610040b10rik"
-d =  DATA[cellType]
+d = DATA[cellType]
 fig = plotGene(d["ann"].etaq,
                np.sqrt(d["fracs"][d["genes"] == gene,:].toarray()[0]),
                d["ann"].mouse)
+
+@app.callback(
+    Output("genePlot", "figure"),
+    [Input("geneSelector", "value"), Input("cellTypeSelector", "value")])
+def updateGeneFigure(gene, cellType):
+    d = DATA[cellType]
+    fig = plotGene(d["ann"].etaq,
+                np.sqrt(d["fracs"][d["genes"] == gene,:].toarray()[0]),
+                d["ann"].mouse)
+    return fig
+
 
 app.layout = html.Div(
     children=[
         html.Div(
             children=[
-                html.H2("Hepatocytes", style={'color': "dodgerblue"}, className="pageh2")
-                ,fig
+                html.H3(id="geneHeader",
+                        children=gene,
+                        style={'color': "dodgerblue"})
+                ,dcc.Graph(id="genePlot")
                 ,cellTypeDrop()
                 ,geneListDrop(d["genes"])
                 ,resultTable(d["detbl"])
--- src/plots.py
+++ src/plots.py
@@ -3,6 +3,5 @@ import dash
 import plotly.express as px
 
 def plotGene(etaq, values, groups):
-    # import pdb; pdb.set_trace()
     fig = px.scatter(x = etaq, y = values, color=groups)
-    return dcc.Graph(figure=fig)
+    return fig
