98c4ab3bd1edd9b372489e531b47199a41a9b2d4
Alexey <alexey.mipt@gmail.com>
2020-11-18 11:34:25 +0100

    zip upd

--- hepapp/reporting.py
+++ hepapp/reporting.py
@@ -6,6 +6,7 @@ import numpy as np
 import plotly
 
 from . import plots
+from . import dataimport
 
 
 def singleGeneScatter(gene, d):
@@ -19,16 +20,33 @@ def singleGeneScatter(gene, d):
                           d["ann"].Genotype)
     return fig
 
-def createGenesZip(genes, d, **kwargs):
+
+def singleGeneLine(gene, betas, spline, d, cellType):
+    genebetas = dataimport.getGeneBetas(betas,  cellType, gene)
+    if genebetas.shape[0] == 0:
+        return None
+    x = dataimport.getMouseSplines(genebetas, spline).melt(id_vars="etaq")
+    mouse2genotype = d['ann'][['mouse', 'Genotype']].drop_duplicates()
+    x = x.join(mouse2genotype.set_index("mouse"), on="mouse")
+    fig = plots.plotGeneLines(x.etaq, x.value, x.mouse, x.Genotype)
+    return fig
+
+def createGenesZip(genes, d, s, cellType, **kwargs):
     """
     Walk through genes, create figures, write to zipped byte array
     """
     f = partial(singleGeneScatter, d=d)
-    figs = {gene:f(gene) for gene in genes}
+    scatterfigs = {gene:f(gene) for gene in genes}
+    f = partial(singleGeneLine, d=d, betas=s['betas'], spline=s['spline'],
+                cellType=cellType)
+    linefigs = {gene:f(gene) for gene in genes}
     buf = io.BytesIO()
     zipbuf = zipfile.ZipFile(buf, "w")
     for gene in genes:
-        with zipbuf.open("{gene}.png".format(gene=gene), "w") as pngfile:
-            figs[gene].write_image(pngfile, "png", **kwargs)
+        with zipbuf.open("{gene}-scatter.png".format(gene=gene), "w") as pngfile:
+            scatterfigs[gene].write_image(pngfile, "png", **kwargs)
+        if linefigs[gene]:
+            with zipbuf.open("{gene}-splines.png".format(gene=gene), "w") as pngfile:
+                linefigs[gene].write_image(pngfile, "png", **kwargs)
     zipbuf.close()
     return buf
