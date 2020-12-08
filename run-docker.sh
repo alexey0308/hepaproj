b50f69fd09185facf40e99a5840e1875934894ca
Alexey <alexey.mipt@gmail.com>
2020-12-08 17:15:02 +0100

    rm files

--- run-docker.sh
+++
@@ -1,8 +0,0 @@
-docker container run -it --rm \
-    -v "$PWD"/data-test:/app/data \
-    -p 127.0.0.1:8001:8000 \
-    --name kiapp \
-    hepapp \
-    gunicorn --reload -b 0.0.0.0:8000 hepapp.app:server --pythonpath=hepapp
-
-# --restart always \
