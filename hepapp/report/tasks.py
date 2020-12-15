b941e7e1e6602bc331ce8e36769e61be44064c3c
Alexey <alexey.mipt@gmail.com>
2020-12-15 16:20:35 +0100

    clean up task

--- hepapp/report/tasks.py
+++ hepapp/report/tasks.py
@@ -1,7 +1,13 @@
 from .celery import app
 from .reporting import writeZipTask
+import os
 
 @app.task
 def plotAll(celltype, genes, filepath):
     writeZipTask(celltype, genes, filepath)
+    deleteFile.apply_async((filepath,), countdown=600)
     return genes
+
+@app.task
+def deleteFile(filepath):
+    os.remove(filepath)
