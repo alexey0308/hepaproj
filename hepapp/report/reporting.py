95277b710ae79d59f07aeb912a74e62573fcfe5d
Alexey <alexey.mipt@gmail.com>
2020-12-15 11:35:29 +0100

    add rabbitmq and py reqs

--- hepapp/report/reporting.py
+++ hepapp/report/reporting.py
@@ -20,9 +20,27 @@ def get_plot(celltype, gene, plot_type):
     return None
 
 
-def write_image_to_zip(zipbuf, filename, fig, **kwargs):
-    with zipbuf.open(filename, "w") as pngfile:
+def write_image_to_buf(buf, filename, fig, **kwargs):
+    with buf.open(filename, "w") as pngfile:
         fig.write_image(pngfile, "png", **kwargs)
+
+def writeZipTask(celltype, genes, fileid):
+    plot_types = ["scatter", "lines"]
+    image_dir = "images"
+    def make_plots(plot_type):
+        return {gene: get_plot(celltype, gene, plot_type) for gene in genes}
+
+    figs = {plot_type: make_plots(plot_type) for plot_type in plot_types}
+    buf = io.FileIO(os.path.join(image_dir, fileid), mode="wb")
+    zipbuf = zipfile.ZipFile(buf, "w")
+    file_number = 0
+    for plot_type in plot_types:
+        for gene, fig in figs[plot_type].items():
+            if fig is not None and len(fig.data) > 0:
+                write_image_to_buf(zipbuf, f"{plot_type}-{gene}.png", fig)
+                file_number += 1
+    zipbuf.close()
+    return fileid
 
 
 def createZip(celltype, genes):
@@ -38,7 +56,7 @@ def createZip(celltype, genes):
     for plot_type in plot_types:
         for gene, fig in figs[plot_type].items():
             if fig is not None and len(fig.data) > 0:
-                write_image_to_zip(zipbuf, f"{plot_type}-{gene}.png", fig)
+                write_image_to_buf(zipbuf, f"{plot_type}-{gene}.png", fig)
                 file_number += 1
     zipbuf.close()
     if file_number == 0:
