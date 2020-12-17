0d41c5d393fb3bc024fa30176cb96d5c7ac9db97
Alexey <alexey.mipt@gmail.com>
2020-12-17 17:49:14 +0100

    longer time

--- hepapp/report/tasks.py
+++ hepapp/report/tasks.py
@@ -5,7 +5,7 @@ import os
 @app.task
 def plotAll(celltype, genes, filepath):
     file_number = writeZipTask(celltype, genes, filepath)
-    deleteFile.apply_async((filepath,), countdown=100)
+    deleteFile.apply_async((filepath,), countdown=600)
     return dict(filepath=filepath, file_number=file_number)
 
 @app.task
